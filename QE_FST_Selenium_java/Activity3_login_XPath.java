8d4a881d1f00236a5730ffdc4a8e382b501863fb
Harshilika <151826195+Harshilika@users.noreply.github.com>
2026-01-31 17:09:52 +0530

    Add files via upload

---
+++ QE_FST_Selenium_java/Activity3_login_XPath.java
@@ -0,0 +1,21 @@
+package demos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity3_login_XPath {
+    public static void main(String[] args) {
+        WebDriver driver=new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/login-form/");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("password");
+        //driver.findElement(By.xpath("//button[@class='svelte-1p
+        // djkmx']")).click();
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+        driver.quit();
+    }
+}
+
+
