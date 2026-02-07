1c1dae677a6affc40e9fba2e6125e73ed6feb2e6
Harshilika <151826195+Harshilika@users.noreply.github.com>
2026-02-07 16:22:15 +0530

    Add files via upload

---
+++ qe_python/Activity21.WebScraping.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+response=requests.get("https://www.training-support.net/webelements/tables").content
+soup=BeautifulSoup(response,"html.parser")
+table=soup.find("table")
+for tr in table.find_all("tr"):
+    cells=tr.find_all("td")
+    row=[i.text for i in cells]
+    print(row)
